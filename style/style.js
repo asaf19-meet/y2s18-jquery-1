58c5184174a657f03d357eef51a894ee9f1ead77
aboud20-meet <aboud20@meet.mit.edu>
2018-08-02 18:35:22 +0300

    jquery style lab

--- style/style.js
+++ style/style.js
@@ -5,10 +5,14 @@ var y = 0; // rotation of Y
 var z = 0; // rotation of Z
 
 function updateSize(newSize) {
+	$("img").eq(0).css("width",newSize)
   // FINISH ME! Set the width of the MEET logo to be newSize in pixels.
 }
 
 function updateRotation() {
+	$("img").eq(0).css("transform","rotateX(" + x.toString() + "deg)" + "rotateY(" + y.toString() + "deg)" + "rotateZ(" + z.toString() + "deg)");
+	//$("img").eq(0).css("transform","rotateY(" + y.toString() + "deg)");
+	//$("img").eq(0).css("transform","rotateZ(" + z.toString() + "deg)");
   // FINISH ME! Set the rotation of the MEET logo based on the values of (x, y, z) (see above!)
 }
 
